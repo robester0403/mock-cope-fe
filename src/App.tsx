c41cec01a59cd1dc5424f6cccfb363c1c8e5d45b
robester0403 <robkso@gmail.com>
2022-09-15 20:47:24 -0400

    added routes and some pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import DailyUpdate from "./pages/DailyUpdate";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dailyupdate" element={<DailyUpdate />}>
+          {/* <Route path="/generalInfo" element={<generalInfo />} />
+          <Route path="/moodUpdate" element={<moodUpdate />} />
+          <Route path="/stressorUpdate" element={<stressorUpdate />} /> */}
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
